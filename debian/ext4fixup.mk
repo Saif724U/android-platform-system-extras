280ccde59c7d47cc392503840458a05289699273
root <root@m36>
2018-03-29 15:00:55 +0530

    fixed dbgsym lintian warning

--- debian/ext4fixup.mk
+++ debian/ext4fixup.mk
@@ -1,14 +1,14 @@
 NAME = ext4fixup
 SOURCES = ext4fixup_main.c
-SOURCES := $(foreach source, $(SOURCES), ext4_utils/$(source))
-CFLAGS += -fno-strict-aliasing 
+SOURCES := $(foreach source, $(SOURCES), ext4_utils/$(source)) 
+CFLAGS +=  -Iext4_utils
 LDFLAGS += -Wl,-rpath=/usr/lib/$(DEB_HOST_MULTIARCH)/android \
-           -Wl,-rpath-link=. \
-	   -L/usr/lib/$(DEB_HOST_MULTIARCH)/android \
+	   -Wl,-rpath-link=. \
+           -L/usr/lib/$(DEB_HOST_MULTIARCH)/android \
            -lz -lsparse -lext4_utils
 
 build: $(SOURCES)
-	$(CC) $^ -o ext4_utils/$(NAME) $(CFLAGS) $(CPPFLAGS) $(LDFLAGS)
+	$(CC) $^ -o ext4_utils/$(NAME) $(CFLAGS) $(LDFLAGS)
 
 clean:
 	$(RM) ext4_utils/$(NAME)
--- debian/mkext4fs.mk
+++ debian/mkext4fs.mk
@@ -1,11 +1,11 @@
 NAME = mkext4fs
-SOURCES = make_ext4fs.c
+SOURCES = make_ext4fs_main.c
 SOURCES := $(foreach source, $(SOURCES), ext4_utils/$(source))
-CFLAGS += -fno-strict-aliasing 
-LDFLAGS += -shared -Wl,-soname,$(NAME).so.0 \
-           -Wl,-rpath=/usr/lib/$(DEB_HOST_MULTIARCH)/android \
-           -L/usr/lib/$(DEB_HOST_MULTIARCH)/android \
-           -lsparse -lselinux
+CFLAGS += -I/usr/include/android/ -DHOST -DANDROID
+LDFLAGS += -Wl,-rpath=/usr/lib/$(DEB_HOST_MULTIARCH)/android \
+	   -Wl,-rpath-link=. \
+	   -L/usr/lib/$(DEB_HOST_MULTIARCH)/android \
+           -lz -lselinux -lsparse -lext4_utils -lcutils
 
 build: $(SOURCES)
 	$(CC) $^ -o ext4_utils/$(NAME) $(CFLAGS) $(CPPFLAGS) $(LDFLAGS)
