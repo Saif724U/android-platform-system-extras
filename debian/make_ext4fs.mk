b2a3792883af901078b0a283dd66d82acd53771f
root <root@m36>
2018-04-10 22:26:53 +0530

    Packaged 4 tools from ext4_utils ext2simg,make_ext4fs,ext4fixup and mkuserimg into android-sdk-ext4-utils, this package breaks and replaces android-tools-fsutils

---
+++ debian/make_ext4fs.mk
@@ -0,0 +1,15 @@
+NAME = make_ext4fs
+SOURCES = make_ext4fs_main.c
+SOURCES := $(foreach source, $(SOURCES), ext4_utils/$(source))
+CFLAGS += -I/usr/include/android/ -DHOST -DANDROID
+LDFLAGS += -Wl,-rpath=/usr/lib/$(DEB_HOST_MULTIARCH)/android \
+	   -Wl,-rpath-link=. \
+	   -L/usr/lib/$(DEB_HOST_MULTIARCH)/android \
+           -lz -lselinux -lsparse -lext4_utils -lcutils
+
+build: $(SOURCES)
+	$(CC) $^ -o ext4_utils/$(NAME) $(CFLAGS) $(LDFLAGS)
+
+clean:
+	$(RM) ext4_utils/$(NAME)
+
